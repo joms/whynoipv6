880aee25f2d0c7796954c03266eb79752e0ad6dd
Jo Emil Holen <jo-emil@outlook.com>
2019-02-06 21:59:36 +0100

    hoooooook(er)s

--- frontend/src/components/CountryList.js
+++ frontend/src/components/CountryList.js
@@ -1,47 +1,45 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import LazyLoad from 'react-lazy-load';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { CountryContext } from '../store/CountryContext';
 
-class CountryList extends React.Component {
-    static contextType = CountryContext;
+const CountryList = props => {
+    const {_didFetch, isFetching, fetchCountries, countries} = useContext(CountryContext);
 
-    componentDidMount() {
-        if (!this.context._didFetch) {
-            this.context.fetchCountries();
+    useEffect(() => {
+        if (!_didFetch && !isFetching) {
+            fetchCountries();
         }
-    }
+    });
 
-    render() {
-        return Object.values(this.context.countries)
-            .sort((a, b) => {
-                const nameA = a.countrycode;
-                const nameB = b.countrycode;
-                if (nameA < nameB) return -1;
-                if (nameA > nameB) return 1;
-                return 0;
-            })
-            .map(country => (
-                <Link to={`${this.props.url}/${country.countrycode.toLowerCase()}`} key={country.countrycode}>
-                    <LazyLoad debounce={false} offsetVertical={500}>
-                        <img
-                            src={`https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.1.0/flags/4x3/${country.countrycode.toLowerCase()}.svg`}
-                            alt={country.countryname}
-                        />
-                    </LazyLoad>
-                    {country.countrycode}
-                </Link>
-            ));
-    }
+    return Object.values(countries)
+        .sort((a, b) => {
+            const nameA = a.countrycode;
+            const nameB = b.countrycode;
+            if (nameA < nameB) return -1;
+            if (nameA > nameB) return 1;
+            return 0;
+        })
+        .map(country => (
+            <Link to={`${props.url}/${country.countrycode.toLowerCase()}`} key={country.countrycode}>
+                <LazyLoad debounce={false} offsetVertical={500}>
+                    <img
+                        src={`https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.1.0/flags/4x3/${country.countrycode.toLowerCase()}.svg`}
+                        alt={country.countryname}
+                    />
+                </LazyLoad>
+                {country.countrycode}
+            </Link>
+        ));
+};
 
-    static defaultProps = {
-        url: '',
-    };
+CountryList.defaultProps = {
+    url: '',
+};
 
-    static propTypes = {
-        url: PropTypes.string,
-    };
-}
+CountryList.propTypes = {
+    url: PropTypes.string,
+};
 
 export default CountryList;
--- frontend/src/components/StatsSummary.js
+++ frontend/src/components/StatsSummary.js
@@ -1,32 +1,27 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { StatsContext } from '../store/StatsContext';
 
-class StatsSummary extends React.Component {
-    static contextType = StatsContext;
+const StatsSummary = () => {
+    const { stats, isFetching, fetchStats } = useContext(StatsContext);
 
-    componentDidMount() {
-        if (!Object.keys(this.context.stats).length && !this.context.isFetching) {
-            this.context.fetchStats();
+    useEffect(() => {
+        if (!Object.keys(stats.all).length && !isFetching) {
+            fetchStats();
         }
+    });
+
+    if (!Object.keys(stats.all).length) {
+        return <>Loading...</>;
     }
 
-    render() {
-        if (!Object.keys(this.context.stats.all).length) {
-            return <React.Fragment />;
-        }
-
-        const { stats } = this.context;
-
-        return (
-            <p>
-                Out of the top 1000 Alexa sites, only {stats.all.top_1k_v6} has IPv6 enabled, and {stats.all.top_1k_v6}{' '}
-                of them use name servers with IPv6 enabled.
-                <br />
-                Of the total {stats.all.sites} sites only {stats.all.percent_v6}% of them have IPv6. This is a huge
-                shame!
-            </p>
-        );
-    }
-}
+    return (
+        <p>
+            Out of the top 1000 Alexa sites, only {stats.all.top_1k_v6} has IPv6 enabled, and {stats.all.top_1k_v6} of
+            them use name servers with IPv6 enabled.
+            <br />
+            Of the total {stats.all.sites} sites only {stats.all.percent_v6}% of them have IPv6. This is a huge shame!
+        </p>
+    );
+};
 
 export default StatsSummary;
